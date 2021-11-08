8a25380311bac01219b62a99ddee464b232d76bf
Colin Broderick <colin.broderick@greeve.co.uk>
2021-11-08 19:22:46 +0000

    Added comma delimiter to python plotter

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-data = np.genfromtxt("data.txt")
+data = np.genfromtxt("data.txt", delimiter=",")
 plt.scatter(data[:,0], data[:,1])
 plt.axis("equal")
 plt.show()
